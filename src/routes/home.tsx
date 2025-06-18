ef4fc0d771b4a32a39d91a1f6a06f6b0a1c965a1
Matheus <mateusinacio32@hotmail.com>
2025-06-18 13:26:12 -0300

    feat: estrutura básica do Vite

---
+++ src/routes/home.tsx
@@ -0,0 +1,13 @@
+import type { Route } from "../app/+types/root";
+import { Welcome } from "../home/home";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export default function Home() {
+  return <Welcome />;
+}
